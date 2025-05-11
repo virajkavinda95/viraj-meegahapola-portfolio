bbc5ce8a48346b8ba8b9eaf3f8428ad309255e65
virakkavinda95 <meegahapola.viraj@gmail.com>
2025-05-11 15:09:31 +0400

    blog issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About/About";
+import Blog from "@/components/Blog/Blog";
 import ContactUs from "@/components/ContactUs/ContactUs";
 import Education from "@/components/Education/Education";
 import Header from "@/components/Header";
